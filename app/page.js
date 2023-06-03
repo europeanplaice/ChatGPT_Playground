88665b033c5f726b55a9405ca260fadbe24a5e07
Tomohiro Endo <europeanplaice@gmail.com>
2023-06-04 04:04:18 +0900

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,131 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React, { useState } from 'react';
+import axios from 'axios';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+const AxiosSWR = async (url, options) => {
+  try {
+    const response = await axios(url, options);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const renderTextWithLineBreaks = (text) => {
+  const lines = text.split('\n');
+  return lines.map((line, index) => (
+    <React.Fragment key={index}>
+      {line}
+      <br />
+    </React.Fragment>
+  ));
+};
+
+
+async function processText(text, data, setData, apiinfo) {
+  const newData = [...data, { "role": "user", "content": text }];
+  const response = await AxiosSWR(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      method: 'POST',
+      headers: {
+        "Authorization": `Bearer ${apiinfo['apikey']}`,
+        "OpenAI-Organization": `${apiinfo['Organization']}`,
+        "Content-Type": "application/json"
+      },
+      data: {
+        "model": "gpt-3.5-turbo",
+        "messages": newData
+      }
+    }
+  );
+  setData((d) => [...d, { "role": "user", "content": text }]);
+  setData((d) => [...d, { "role": "assistant", "content": response.choices[0].message.content }]);
+}
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [error, setError] = useState(null); 
+  const [apiinfo, setApiinfo] = useState({'Organization': "", 'apikey': ""}); 
+
+  const handleSubmit = async () => {
+    setIsSending(true);
+    setError(null);
+    try {
+      await processText(inputText, data, setData, apiinfo);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsSending(false);
+      setInputText("");
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Container maxWidth="md">
+      <h1>Test ChatGPT</h1>
+      <Accordion sx={{marginBottom: '1em', maxWidth: '60%'}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >API Config</AccordionSummary>
+        <AccordionDetails>
+          <TextField 
+            label='Organization ID' 
+            type="password" 
+            sx={{marginRight: '4px'}} 
+            value={apiinfo['Organization']}
+            onChange={(e) => setApiinfo((info) => ({...info, "Organization": e.target.value}))}
+            ></TextField>
+          <TextField 
+            label='API KEY' 
+            type="password"
+            value={apiinfo['apikey']}
+            onChange={(e) => setApiinfo((info) => ({...info, "apikey": e.target.value}))}
+            ></TextField>
+        </AccordionDetails>
+      </Accordion>
+      {error && <Alert severity="error">{error}</Alert>}
+      <Box sx={{ margin: '1em' }}>
+        {data.map((d, index) => {
+          return (
+            <Box key={index}>
+              <h2>{d.role}</h2>
+              <Box>
+                {renderTextWithLineBreaks(d.content)}
+              </Box>
+              <hr></hr>
+            </Box>
+          )
+        })}
+        {isSending ? <CircularProgress color="secondary" /> : ""}
+      </Box>
+      <Box>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="message"
+          multiline
+          maxRows={100}
+          fullWidth
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          disabled={isSending} 
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Button sx={{margin: '1em'}}
+          onClick={handleSubmit} variant="contained" disabled={isSending} >{isSending ? "sending..." : "send"}</Button>
+      </Box>
+    </Container>
+  );
 }
